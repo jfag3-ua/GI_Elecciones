b8a81ad54b030175cbb9fd93ca2814390b59fc16
Alberto MP <amp260@alu.ua.es>
2025-03-26 23:42:04 +0100

    Tablas de datos historicos

--- elecciones/resources/views/resultados.blade.php
+++ elecciones/resources/views/resultados.blade.php
@@ -28,7 +28,7 @@
     <body>
         <h1>Información General de Elecciones</h1>
         @foreach ($years as $year)
-            @if (isset($info_votos[$year]))
+            @if (isset($info_general[$year]))
                 <h2>Año: {{ $year }}</h2>
                 <table>
                     <tr>
@@ -40,17 +40,17 @@
                         <th>A Candidaturas</th>
                         <th>En Blanco</th>
                     </tr>
-                    @foreach ($info_votos[$year] as $votos)
-                        <tr>
-                            <td>{{ $info_votos[$year]['Censo'] ?? 0 }}</td>
+                    <tr>
+                        @foreach ($info_general[$year] as $votos)
+                            <td>{{ $votos['Censo'] ?? 0 }}</td>
                             <td>{{ $votantes = $votos['Votantes'] ?? 0 }}</td>
                             <td>{{ $votos['Abstenciones'] ?? 0 }}</td>
                             <td>{{ $voto_valido = $votos['Validos'] ?? 0 }}</td>
                             <td>{{ $votantes - $voto_valido }}</td>
                             <td>{{ $votos['A candidaturas'] ?? 0 }}</td>
                             <td>{{ $votos['En blanco'] ?? 0 }}</td>
-                        </tr>
-                    @endforeach
+                        @endforeach
+                    </tr>
                 </table>
             @endif
         @endforeach
@@ -63,18 +63,17 @@
                     <tr>
                         <th>Candidato</th>
                         <th>Votos</th>
-                        <th>Porcentaje</th>
                         <th>Escaños</th>
                     </tr>
+
                     @foreach ($info_votos[$year] as $candidatura)
-                        @foreach ($candidatura as $candidato)
-                            <tr>
-                                <td>{{ $candidato['Candidato'] ?? '/NA' }}</td>
-                                <td>{{ $candidato['Votos'] ?? 1 }}</td>
-                                <td>{{ number_format(($candidato['Votos'] ?? 1) * 100 / ($info_general[$year]['Votantes'] ?? 1), 2, ',', '.') }}%</td>
-                                <td>{{ $candidato['Escanos'] ?? 'N/A' }}</td>
-                            </tr>
-                        @endforeach
+                            @foreach ( $candidatura as $key => $value)
+                                <tr>
+                                    @foreach ($value as $index =>$result)
+                                        <td>{{ $result }}</td>
+                                    @endforeach
+                                </tr>
+                            @endforeach
                     @endforeach
                 </table>
             @endif
