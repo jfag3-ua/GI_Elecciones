bace1b5ea5ad2253f975a96274d4d5275b44dae0
Juan <jfag3-ua@alu.ua.es>
2025-03-28 20:35:09 +0100

    Cambios de estilo y visibilidad de vistas

--- elecciones/resources/views/landing.blade.php
+++ elecciones/resources/views/landing.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'Eleccions Valencianes')
 
 @section('content')
-    <h2>Eleccions a les Corts Valencianes</h2>
-    <p class="notice">Aquí se dará la bienvenida al sistema de voto.</p>
+    <h2>Eleccions a Les Corts Valencianes</h2>
+    <p class="notice">
+        Bienvenido al sistema de voto electrónico de <b>Les Corts Valencianes</b>.
+    </p>
+    <p class="notice">
+        Ten en cuenta que para poder votar necesitas iniciar sesión.
+        Si aún no tienes una cuenta, debes registrarte, para lo cual es imprescindible que estés censado, si no, el sistema no te permitirá registrarte.
+    </p>
     <img src="/img/Corts_Valencianes.png" />
 @endsection
