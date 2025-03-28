fe3830534d415bafebff28d3783849886ba30334
Alberto MP <amp260@alu.ua.es>
2025-03-28 20:19:34 +0100

    Tablas Historicas creadas

--- elecciones/app/Http/Controllers/PaginaController.php
+++ elecciones/app/Http/Controllers/PaginaController.php
@@ -38,14 +38,26 @@ class PaginaController extends Controller
         $csv = Reader::createFromPath(storage_path('/app/datasets/elections_dataset.csv'), 'r');
         $csv->setHeaderOffset(0); // Usa la primera fila como encabezados
     
-        $years = $csv->getHeader(); // Obtener los años como columnas
+        // $years = $csv->getHeader(); // Obtener los años como columnas
+        $years = array_reverse($csv->getHeader()); // Invertir el orden de los años
         $info_general = [];
         $info_votos = [];
     
         foreach ($csv as $index => $row) {
             foreach ($years as $year) {
+                // Reemplazar caracteres especiales en el JSON
+                $replacements = [
+                    "'"  => '"',   
+                    "d'"  => "d ",
+                ];
+
                 // Decodificar los valores almacenados como diccionario en el CSV
-                $dictionary = json_decode(str_replace("'", '"', $row[$year]), true) ?? [];
+                $dictionary = json_decode(strtr($row[$year], $replacements), true);
+
+                // Verificar si el JSON se decodificó correctamente
+                if (!is_array($dictionary)) {
+                    dd("Error en JSON en el año: $year", $row[$year], json_last_error_msg(), $dictionary);
+                }
     
                 if ($index == 1) { // Primera fila -> Info General
                     $info_general[$year][] = $dictionary;
@@ -53,9 +65,14 @@ class PaginaController extends Controller
                  
                 elseif ($index == 2) { // Resto de filas -> Info Votos
                     $info_votos[$year][] = $dictionary;
+                    //dd("Valor de la ARRAY: $year", $row[$year], $dictionary);
                 }
             }
         }
+
+        // Invertir el orden de los datos para que coincidan con los años invertidos
+        $info_general = array_reverse($info_general, true);
+        $info_votos = array_reverse($info_votos, true);
     
         return view('resultados', compact('info_general', 'info_votos', 'years'));
     }
