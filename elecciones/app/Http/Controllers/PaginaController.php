5134d0984f70d778664e7521683eab8beab2ba18
Alberto MP <amp260@alu.ua.es>
2025-03-29 21:02:45 +0100

    Resultados y Predicciones

--- elecciones/app/Http/Controllers/PaginaController.php
+++ elecciones/app/Http/Controllers/PaginaController.php
@@ -26,7 +26,49 @@ class PaginaController extends Controller
     }
 
     public function encuestas() {
-        return view('encuestas');
+        if (!file_exists(storage_path('/app/datasets/predictions_dataset.csv'))) {
+            return redirect()->route('inicio')->with('error', 'No se ha cargado el dataset');
+        }
+
+        $csv = Reader::createFromPath(storage_path('/app/datasets/predictions_dataset.csv'), 'r');
+        $csv->setHeaderOffset(0); // Usa la primera fila como encabezados
+    
+        // $years = $csv->getHeader(); // Obtener los años como columnas
+        $years = array_reverse($csv->getHeader()); // Invertir el orden de los años
+        $abstencion = [];
+        $blanco = [];
+        $nulo = [];
+    
+        foreach ($csv as $index => $row) {
+            foreach ($years as $year) {
+                // Reemplazar caracteres especiales en el JSON
+                $replacements = [
+                    "'"  => '"',   
+                ];
+
+                // Decodificar los valores almacenados como diccionario en el CSV
+                $dictionary = json_decode(strtr($row[$year], $replacements), true);
+    
+                if ($index == 1) {
+                    $abstencion[$year][] = $dictionary;
+                }
+                 
+                elseif ($index == 2) {
+                    $blanco[$year][] = $dictionary;
+                }
+
+                elseif ($index == 3) {
+                    $nulo[$year][] = $dictionary;
+                }
+            }
+        }
+
+        // Invertir el orden de los datos para que coincidan con los años invertidos
+        $abstencion = array_reverse($abstencion, true);
+        $blanco = array_reverse($blanco, true);
+        $nulo = array_reverse($nulo, true);
+
+        return view('encuestas', compact('abstencion', 'blanco', 'nulo', 'years'));
     }
 
     public function resultados()
@@ -42,6 +84,7 @@ class PaginaController extends Controller
         $years = array_reverse($csv->getHeader()); // Invertir el orden de los años
         $info_general = [];
         $info_votos = [];
+        $winners = [];
     
         foreach ($csv as $index => $row) {
             foreach ($years as $year) {
@@ -73,8 +116,44 @@ class PaginaController extends Controller
         // Invertir el orden de los datos para que coincidan con los años invertidos
         $info_general = array_reverse($info_general, true);
         $info_votos = array_reverse($info_votos, true);
+        
+        foreach ($years as $year) {
+            foreach ($info_votos[$year] as $candidatura) {
+                $candidatos_list = $candidatura['Candidato'] ?? [];
+                $votaciones_list = $candidatura['Votos'] ?? [];
+                $escanos_list = $candidatura['Escanos'] ?? [];
+                $count = max(count($candidatos_list), count($votaciones_list), count($escanos_list));
+
+                $elected = [];
+                for ($i = 0; $i < $count; $i++) {
+                    $candidato = $candidatos_list[$i] ?? '/NA';
+                    $votacion = $votaciones_list[$i] ?? 0;
+                    $escano = $escanos_list[$i] ?? 0;
+
+                    if ($escano > 0){
+                        $elected[$candidato] = $escano;
+                    }
+                }
+
+                $data = [
+                    // Partidos
+                    'labels' => array_keys($elected),
+        
+                    // Escaños
+                    'datasets' => [[
+                        'label' => 'Total de Escaños',
+                        'data' => $elected,
+                        'backgroundColor' => 'rgba(54, 162, 235, 0.5)',
+                        'borderColor' => 'rgba(54, 162, 235, 1)',
+                        'borderWidth' => 1
+                    ]]
+                ];
+
+                $winners[$year] = $data;
+            }
+        }
     
-        return view('resultados', compact('info_general', 'info_votos', 'years'));
+        return view('resultados', compact('info_general', 'info_votos', 'years', 'winners'));
     }
 
     public function administracion() {
--- elecciones/resources/views/resultados.blade.php
+++ elecciones/resources/views/resultados.blade.php
@@ -3,148 +3,243 @@
 @section('title', 'Resultados')
 
 @section('content')
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        .label-column {
-            text-align: left;
-        }
-        .divider-row {
-            border-bottom: 3px solid black;
-        }
-    </style>
+    <!DOCTYPE html>
+    <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <h2>Resultados</h2>
+            <title>Resultados</title>
+
+            <style>
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-bottom: 20px;
+                }
+                th, td {
+                    border: 1px solid black;
+                    padding: 8px;
+                    text-align: left;
+                }
+                th {
+                    background-color: #f2f2f2;
+                }
+                .label-column {
+                    text-align: left;
+                }
+                .divider-row {
+                    border-bottom: 3px solid black;
+                }
+
+                .text-porcentaje, .text-escano {
+                    color: #a41336ff;
+                    font-weight: bold;
+                }
+            </style>
+
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        </head>
     
-    @php
-        $votantes = [];
-    @endphp
+        @php
+            $votantes = [];
+        @endphp
 
-    @foreach ($years as $year)
-        @if (isset($info_general[$year]))
-            <h3>Información general sobre las elecciones de {{ $year }}</h3>
-            <table class="responsive-enabled table table-hover table-striped" data-striping="1" data-once="tableresponsive">
-                @foreach ($info_general[$year] as $votos)
-                    <thead>
-                        <tr>
-                            <th colspan="3">Información general sobre las elecciones</th>
-                            <th class="text-right">Número</th>
-                            <th class="text-right">Porcentaje</th>
-                        </tr>
-                    </thead>
+        <body>
+            <h1>Resultados</h1>
+            @foreach ($years as $year)
 
-                    <tbody>
-                        <tr>
-                            <td rowspan="2">Censo</td>
-                            <td rowspan="2">{{ number_format($votos['Censo'] ?? 0, 0, ',', '.') }}</td>
-                            <td>Votantes</td>
-                            <td class="text-right">{{ number_format($votos['Votantes'] ?? 0, 0, ',', '.') }}</td>
-                            <td class="text-right">{{ number_format(($votos['Votantes'] ?? 0) * 100 / ($votos['Censo'] ?? 0), 2, ',', '.') }}%</td>
-                        </tr>
+                <h2>Información general sobre las elecciones de {{ $year }}</h2>
 
-                        @php
-                            $votantes[$year] = $votos['Votantes'] ?? 0;
-                        @endphp
+                @php
+                    $data = json_encode($winners[$year] ?? []);
+                    //dd($data);
+                @endphp
 
-                        <tr>
-                            <td>Abstenciones</td>
-                            <td class="text-right">{{ number_format($votos['Abstenciones'] ?? 0, 0, ',', '.') }}</td>
-                            <td class="text-right">{{ number_format(($votos['Abstenciones'] ?? 0) * 100 / ($votos['Censo'] ?? 0), 2, ',', '.') }}%</td>
-                        </tr>
-
-                        <tr>
-                            <td rowspan="2">Válidos</td>
-                            <td rowspan="2">{{ number_format($votos['Validos'] ?? 0, 0, ',', '.') }}</td>
-                            <td>A candidaturas</td>
-                            <td class="text-right">{{ number_format($votos['A candidaturas'] ?? 0, 0, ',', '.') }}</td>
-                            <td class="text-right">{{ number_format(($votos['A candidaturas'] ?? 0) * 100 / ($votos['Validos'] ?? 0), 2, ',', '.') }}%</td>
-                        </tr>
-
-                        <tr class="even">
-                            <td>En blanco</td>
-                            <td class="text-right">{{ number_format($votos['En blanco'] ?? 0, 0, ',', '.') }}</td>
-                            <td class="text-right">{{ number_format(($votos['En blanco'] ?? 0) * 100 / ($votos['Validos'] ?? 0), 2, ',', '.') }}%</td>
-                        </tr>
-
-                        <tr class="even">
-                            <td rowspan="2" colspan="3">Nulos</td>
-                        </tr>
-                        <tr class="odd">
-                            <td rowspan="1" colspan="1">{{ number_format($votantes[$year] - $votos['Validos'] ?? 0, 0, ',', '.') }}</td>
-                            <td class="text-right"> - </td>
-                        </tr>
-                    </tbody>
-                @endforeach
-            </table>
-
-            <table>
-                <tr>
-                    <th>Censo</th>
-                    <th>Votantes</th>
-                    <th>Abstenciones</th>
-                    <th>Válidos</th>
-                    <th>Nulos</th>
-                    <th>A candidaturas</th>
-                    <th>En blanco</th>
-                </tr>
-                <tr>
+                @if (isset($info_general[$year]))
                     @foreach ($info_general[$year] as $votos)
-                        <td>{{ number_format($votos['Censo'] ?? 0, 0, ',', '.') }}</td>
-                        <td>{{ number_format($votos['Votantes'] ?? 0, 0, ',', '.') }}</td>
-
                         @php
                             $votantes[$year] = $votos['Votantes'] ?? 0;
                         @endphp
 
-                        <td>{{ number_format($votos['Abstenciones'] ?? 0, 0, ',', '.') }}</td>
-                        <td>{{ number_format($votos['Validos'] ?? 0, 0, ',', '.') }}</td>
-                        <td>{{ number_format($votantes[$year] - $votos['Validos'] ?? 0, 0, ',', '.') }}</td>
-                        <td>{{ number_format($votos['A candidaturas'] ?? 0, 0, ',', '.') }}</td>
-                        <td>{{ number_format($votos['En blanco'] ?? 0, 0, ',', '.') }}</td>
+                        
+                        <canvas id="mychart"></canvas>
+
+                        <script>
+                            document.addEventListener("DOMContentLoaded", function() {
+                                const ctx = document.getElementById('mychart').getContext('2d');
+
+                                // Pasar datos de PHP a JavaScript
+                                const data = {
+                                    labels: ["Partido Popular (PP)", "Partido Socialista Obrero Español (PSOE)", 
+                                    "Compromís: Més-Iniciativa-VerdsEquo (COMPROMÍS)", "VOX"],
+                                    
+                                    datasets: [{
+                                        data: [50, 30, 12, 7], // Número de escaños
+                                        backgroundColor: ["blue", "red", "orange", "green"]
+                                    }
+                                ]};
+
+                                const options = {
+                                    responsive: true,
+                                    cutout: "60%", // Hace que parezca un gráfico semicircular
+                                    rotation: -90, // Ajuste de inicio
+                                    circumference: 180, // Hace que sea semicircular
+                                    plugins: {
+                                        legend: {
+                                            position: "top",
+                                            labels: {
+                                                usePointStyle: true
+                                            }
+                                        },
+
+                                        // Configuración para mostrar los valores
+                                        datalabels: {
+                                            color: '#fff',
+                                            font: {
+                                                weight: 'bold',
+                                                size: 14
+                                            },
+                                            
+                                            formatter: (value) => {
+                                                return value;
+                                            },
+                                            anchor: 'end',
+                                            align: 'start',
+                                            offset: -30
+                                        }
+                                    }
+                                };
+
+                                new Chart(ctx, {
+                                    type: 'doughnut',
+                                    data: data,
+                                    options: options
+                                });
+                            });
+                        </script>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th colspan="3">Información general sobre las elecciones</th>
+                                    <th>Número</th>
+                                    <th>Porcentaje</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="odd">
+                                    <td rowspan="2">Censo</td>
+                                    <td class="text-numero" rowspan="2">
+                                        {{ number_format($votos['Censo'] ?? 0, 0, ',', '.') }}
+                                    </td>
+
+                                    <td>Votantes</td>
+                                    <td class="text-numero">
+                                        {{ number_format($votos['Votantes'] ?? 0, 0, ',', '.') }}
+                                    </td>
+                                    <td class="text-porcentaje">
+                                        {{ number_format(($votos['Votantes'] ?? 0) * 100 / ($votos['Censo'] ?? 0), 2, ',', '.') }}%
+                                    </td>
+                                </tr>
+
+                                <tr class="even">
+                                    <td>Abstenciones</td>
+                                    <td class="text-numero">
+                                        {{ number_format($votos['Abstenciones'] ?? 0, 0, ',', '.') }}
+                                    </td>
+                                    <td class="text-porcentaje">
+                                        {{ number_format(($votos['Abstenciones'] ?? 0) * 100 / ($votos['Censo'] ?? 0), 2, ',', '.') }}%
+                                    </td>
+                                </tr>
+
+                                <tr class="odd">
+                                    <td rowspan="2">Votos Válidos</td>
+                                    <td class="text-numero" rowspan="2">
+                                        {{ number_format($votos['Validos'] ?? 0, 0, ',', '.') }}
+                                    </td>
+
+                                    <td>A candidaturas</td>
+                                    <td class="text-numero">
+                                        {{ number_format($votos['A candidaturas'] ?? 0, 0, ',', '.') }}
+                                    </td>
+                                    <td class="text-porcentaje">
+                                        {{ number_format(($votos['A candidaturas'] ?? 0) * 100 / ($votos['Validos'] ?? 0), 2, ',', '.') }}%
+                                    </td>
+                                </tr>
+
+                                <tr class="even">
+                                    <td>En blanco</td>
+                                    <td class="text-numero">
+                                        {{ number_format($votos['En blanco'] ?? 0, 0, ',', '.') }}
+                                    </td>
+                                    <td class="text-porcentaje">
+                                        {{ number_format(($votos['En blanco'] ?? 0) * 100 / ($votos['Validos'] ?? 0), 2, ',', '.') }}%
+                                    </td>
+                                </tr>
+
+                                <tr class="odd">
+                                    <td rowspan="2"> Votos Nulos</td>
+                                    <td rowspan="2">
+                                        {{ number_format($votantes[$year] - $votos['Validos'] ?? 0, 0, ',', '.') }}
+                                    </td>
+                                    
+                                    <td class="text-numero" style="text-align: center;" colspan="3"> - </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     @endforeach
-                </tr>
-            </table>
-        @endif
-    
-        <h3>Resultados de las elecciones de {{ $year }}</h3>
-        @if (isset($info_votos[$year]))
-            <table>
-                <tr>
-                    <th>Candidato</th>
-                    <th>Votos</th>
-                    <th>Porcentaje</th>
-                    <th>Escaños</th>
-                </tr>
+                @endif
+            
+                <h2>Resultados de las elecciones de {{ $year }}</h2>
 
-                @foreach ($info_votos[$year] as $candidatura)
-                    @php
-                        $candidatos = $candidatura['Candidato'] ?? [];
-                        $votaciones = $candidatura['Votos'] ?? [];
-                        $escanos = $candidatura['Escanos'] ?? [];
-                        $count = max(count($candidatos), count($votaciones), count($escanos));
-                    @endphp
+                @if (isset($info_votos[$year]))
+                    @foreach ($info_votos[$year] as $candidatura)
+                        @php
+                            $candidatos = $candidatura['Candidato'] ?? [];
+                            $votaciones = $candidatura['Votos'] ?? [];
+                            $escanos = $candidatura['Escanos'] ?? [];
+                            $count = max(count($candidatos), count($votaciones), count($escanos));
+                        @endphp
 
-                    @for ($i = 0; $i < $count; $i++)
-                        <tr>
-                            <td>{{ $candidatos[$i] ?? '/NA' }}</td>
-                            <td>{{ number_format($votaciones[$i] ?? 0, 0, ',', '.') }}</td>
-                            <td>{{ number_format(($votaciones[$i] * 100) / $votantes[$year], 2, ',', '.') }}%</td>
-                            <td>{{ $escanos[$i] ?? 0 }}</td>
-                        </tr>
-                    @endfor
-                @endforeach
-            </table>
-        @endif
-    @endforeach
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Candidato</th>
+                                    <th>Votos</th>
+                                    <th>Porcentaje</th>
+                                    <th>Escaños</th>
+                                </tr>
+                            </thead>
+                            <tbody>                  
+                                @for ($i = 0; $i < $count; $i++)
+                                    <tr>
+                                        <td>
+                                            {{ $candidatos[$i] ?? '/NA' }}
+                                        </td>
+
+                                        <td class="text-numero">
+                                            {{ number_format($votaciones[$i] ?? 0, 0, ',', '.') }}
+                                        </td>
+
+                                        <td>
+                                            {{ number_format(($votaciones[$i] * 100) / $votantes[$year], 2, ',', '.') }}%
+                                        </td>
+
+                                        <td class="text-escano">
+                                            {{ $escanos[$i] ?? 0 }}
+                                        </td>
+                                    </tr>
+                                @endfor
+                            </tbody>
+                        </table>
+
+                    @endforeach
+                @endif
+
+            @endforeach
+
+        </body>
+    </html>
 @endsection
