575b58f55578f17965c1918c77c445cbf1ca8390
pmr71-ua <pmr71@alu.ua.es>
2025-06-04 13:21:42 -0300

    cambio de orden partidos

--- elecciones/app/Http/Controllers/CandidatoController.php
+++ elecciones/app/Http/Controllers/CandidatoController.php
@@ -271,7 +271,7 @@ class CandidatoController
                 'cand.nombre as nombrePartido',
                 'cand.color'
             )
-            ->orderBy('nombrePartido')
+            ->orderBy('cand.idCandidatura')
             ->orderBy('c.orden')
             ->get()
             ->groupBy('nombrePartido');
